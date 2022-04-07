ee476c8a4326f3bfaae049ab4c1bc33e7e69e67f
Natalia Rudenko <run@crtweb.ru>
2022-04-07 11:43:03 +0500

    changing of main

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,9 @@ import { useSelector, useDispatch } from "react-redux";
 import getPhotos from "../../store/actions/getPhotos";
 import { toggleTheme } from "../../store/actions/toggleTheme";
 import { themeSelector } from "../../store/selectors";
-import { DOWNLOAD_DATA, THEME } from "./store";
-import Login from "../Login/Login";
+
+import { API_KEY, REDIRECT_URI } from "../../Utils/constants";
+import { DOWNLOAD_DATA, THEME, AUTH } from "./store";
 
 const Header = () => {
   const theme = useSelector(themeSelector);
@@ -18,8 +19,26 @@ const Header = () => {
     dispatch(toggleTheme());
   }, [dispatch]);
 
+  const [client_id, redirect_uri, response_type, scope] = [
+    API_KEY,
+    REDIRECT_URI,
+    "code",
+    "public+write_likes",
+  ];
+
+  const beginAutorisation = () => {
+    console.log();
+    // const paramsURI = new URLSearchParams(params).toString();
+    const authenticationUrl = `https://unsplash.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}&scope=${scope}`;
+    console.log("authenticationUrl", authenticationUrl);
+    document.location.assign(authenticationUrl);
+  };
+
   return (
     <>
+      <button className={`button ${theme}`} onClick={beginAutorisation}>
+        {AUTH}
+      </button>
       <button className={`button ${theme}`} onClick={toggleThemeHandler}>
         {THEME}
       </button>
